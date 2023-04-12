993153a983e066419d181f1870a7b037b45dd5a5
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-04-13 01:19:26 +0200

    add  backgroundColor

--- lib/screens/add_post.dart
+++ lib/screens/add_post.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_app/shered/colors.dart';
 
 class AddPost extends StatefulWidget {
   const AddPost({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class _AddPostState extends State<AddPost> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: mobileBackgroundColor,
       appBar: AppBar(
         title: Text("AddPost Screen"),
       ),
