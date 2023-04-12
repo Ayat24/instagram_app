442803ad88bd4a33cd56b698b82b661f29103d49
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-04-12 17:58:03 +0200

    add title image and color to home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_app/shered/colors.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,7 +13,11 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("home screen"),
+        title: SvgPicture.asset(
+          "assets/img/instagram.svg",
+          color: primaryColor,
+          height: 32,
+        ),
       ),
     );
   }
