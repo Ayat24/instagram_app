97684d9179c013bdcb71fc088c2e47d4c46bcce3
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-04-13 00:36:44 +0200

    addmargin , width,and text to container

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -114,7 +114,9 @@ class _ProfileState extends State<Profile> {
             ],
           ),
  Container(
-              ),
+              margin: EdgeInsets.fromLTRB(15, 21, 0, 0),
+              width: double.infinity,
+              child: Text(" Cute & nice girl")),
 
    
     );
