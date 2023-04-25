260136d3b0eebf537ccdd82b90e88db1017173f3
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-04-25 22:22:55 +0200

    add text field and text and icons in body
    Signed-off-by: Ayat24 <126516771+Ayat24@users.noreply.github.com>

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -30,11 +30,92 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     // final googleSignInProvider = Provider.of<GoogleSignInProvider>(context);
     return Scaffold(
-      backgroundColor: mobileBackgroundColor,
-      appBar: AppBar(
-        // backgroundColor: appbarGreen,
-        title: Text("Sign in"),
-      ),
-    );
+        backgroundColor: mobileBackgroundColor,
+        appBar: AppBar(
+          // backgroundColor: appbarGreen,
+          title: Text("Sign in"),
+        ),
+        body: Center(
+            child: Padding(
+          padding: const EdgeInsets.all(33.0),
+          child: SingleChildScrollView(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const SizedBox(
+                height: 64,
+              ),
+              TextField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: false,
+                  decoration: decorationTextfield.copyWith(
+                      hintText: "Enter Your Email : ",
+                      suffixIcon: Icon(Icons.email))),
+              const SizedBox(
+                height: 33,
+              ),
+              TextField(
+                  controller: passwordController,
+                  keyboardType: TextInputType.text,
+                  obscureText: isVisable ? false : true,
+                  decoration: decorationTextfield.copyWith(
+                      hintText: "Enter Your Password : ",
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isVisable = !isVisable;
+                            });
+                          },
+                          icon: isVisable
+                              ? Icon(Icons.visibility)
+                              : Icon(Icons.visibility_off)))),
+              const SizedBox(
+                height: 33,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  // await signIn();
+                  // if (!mounted) return;
+                  // showSnackBar(context, "Done ... ");
+                },
+                style: ButtonStyle(
+                  // backgroundColor: MaterialStateProperty.all(BTNgreen),
+                  padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+                ),
+                child: isLoading
+                    ? CircularProgressIndicator(
+                        color: Colors.white,
+                      )
+                    : Text(
+                        "Sign in",
+                        style: TextStyle(fontSize: 19),
+                      ),
+              ),
+              const SizedBox(
+                height: 9,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Do not have an account?",
+                      style: TextStyle(fontSize: 18)),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => Register()),
+                        );
+                      },
+                      child: Text('sign up',
+                          style: TextStyle(
+                              fontSize: 18,
+                              decoration: TextDecoration.underline))),
+                ],
+              ),
+            ]),
+          ),
+        )));
   }
 }
